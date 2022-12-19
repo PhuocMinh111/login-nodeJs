9fb10ca5be8b8f950421e30618b91a6fd00bee32
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-12-19 19:08:07 +0700

    views template

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const path = require('path');
 const {engine}  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
 app.engine('handlebars',engine());
 app.set("view engine",'handlebars');
 app.set('views',path.join(__dirname,'./views'));
+
+
 
 const PORT = 3001;
 app.get('/',(req,res)=> {
     res.render('home');
 })
-
+app.post("/",(req,res)=> {
+    console.log(req.body);
+})
 
 
 
